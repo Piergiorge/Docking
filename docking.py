eeebbe2e2365435e60c5a8db518e3766f0caa1cf
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2023-01-10 12:28:39 -0300

    Update docking.py

--- docking.py
+++ docking.py
@@ -1,5 +1,19 @@
 import pybel
 
+"""
+This code is using the Pybel library, which is a
+Python wrapper for the Open Babel toolkit, to
+perform a molecular docking simulation. The script
+loads a ligand and a receptor molecules from the files
+ligand.mol2 and receptor.pdb respectively. Then it sets
+up the input for the docking simulation by creating a Pybel
+Docking object, where the ligand and receptor molecules are
+set as the ligand and receptor properties. After that, the
+script runs the docking simulation by calling the run() method 
+of the Docking object and finally, prints the energy of the
+final complex using the energy attribute of the result object.
+"""
+
 # Load the ligand and receptor molecules
 ligand = pybel.readfile("mol2", "ligand.mol2").next()
 receptor = pybel.readfile("pdb", "receptor.pdb").next()
