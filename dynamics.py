38bab9171bdddc5483cdf03aef30f57bb2e1e4b5
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2023-01-10 12:31:01 -0300

    Update dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -1,5 +1,22 @@
 import mdtraj as md
 
+"""
+This code uses the mdtraj library to simulate the motion of a system over time.
+It first loads the topology and coordinates for the system from a PDB file and
+a DCD file, respectively. The topology is used to describe the connectivity and
+types of atoms in the system, while the coordinates represent the initial positions
+of these atoms.
+
+The code then sets up the simulation parameters such as temperature, friction,
+and timestep. A Langevin integrator is used to simulate the system dynamics. This
+is a type of numerical integration technique that combines the Newtonian dynamics
+of the system with a random force, known as a "thermostat", to mimic the effects of
+a heat bath on the system.
+
+The simulation is run for 1000 steps and the final coordinates of the atoms are
+saved to a new DCD file named "final.dcd". The final coordinates represents the
+position of atoms after the simulation has ended.
+"""
 # Load the topology and coordinates for the system
 topology = md.load("system.pdb").topology
 coordinates = md.load("system.dcd", top=topology).xyz
